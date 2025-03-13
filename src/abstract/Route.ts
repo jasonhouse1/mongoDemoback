0646683082ba4fd3af723540bad221777c200df7
Mr.C <112913104+jasonhouse1@users.noreply.github.com>
2025-03-13 22:07:07 +0800

    Update Route.ts

--- src/abstract/Route.ts
+++ src/abstract/Route.ts
@@ -3,22 +3,18 @@ import { Contorller } from "./Contorller"
 
 export abstract class Route{
 
-    // 抽象屬性 - 路由基礎 URL
-    protected abstract url: string;
+    protected abstract url:string
 
-    // 抽象屬性 - 與該路由相關的控制器
-    protected abstract controller: Controller;
+    protected abstract Contorller:Contorller
 
-    // Express 路由器實例
-    protected router = Router();
+    protected router = Router()
 
-    // 設定路由 - 子類必須實現
-    protected abstract setRoutes(): void;
-    // 公共方法 - 獲取路由器
+    protected abstract setRoutes(): void
+
     public getRouter(){
         return this.router
     }
-    // 公共方法 - 獲取路由基礎 URl
+
     public getUrl(){
         return this.url
     }
